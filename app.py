ab612b26e1f0378810bafe152a621c04c25fad0d
David Majekodunmi <87044724+davidfmajek@users.noreply.github.com>
2025-02-22 18:17:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def favorites_page():
     return render_template('favorites.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
